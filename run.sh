cac364b698b1275e5e922140fbe19ff2c11b3657
Steven Lazidis <the1laz@users.noreply.github.com>
2017-07-27 23:08:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 VERSION=7.8.1
-DOCKERHUBUSER=heziegl
 
-sudo docker run -d \
-		-p 9000:9000 \
-                -p 9090:9090 \
-		-p 3483:3483 \
-		-p 3483:3483/udp \
-		--hostname="$HOSTNAME-docker-lms" \
-		-v "/etc/localtime":"/etc/localtime":ro \
-		-v $(pwd)/vol_lms_persist:/var/lib/squeezeboxserver \
-		-v $(pwd)/vol_lms_log:/var/log/squeezeboxserver \
-		-v $(pwd)/vol_lms_media:/media \
-		--name="lms" \
-		$DOCKERHUBUSER/rpi-lms:$VERSION
+#sudo docker run -d \
+#		-p 9000:9000 \
+#                -p 9090:9090 \
+#		-p 3483:3483 \
+#		-p 3483:3483/udp \
+#		--hostname="$HOSTNAME-docker-lms" \
+#		-v "/etc/localtime":"/etc/localtime":ro \
+#		-v $(pwd)/vol_lms_persist:/var/lib/squeezeboxserver \
+#		-v $(pwd)/vol_lms_log:/var/log/squeezeboxserver \
+#		-v $(pwd)/vol_lms_media:/media \
+#		--name="lms" \
+#		$DOCKERHUBUSER/rpi-lms:$VERSION
