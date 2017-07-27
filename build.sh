a5c83ddf2d440547a603f68f91b61041461e1125
Steven Lazidis <the1laz@users.noreply.github.com>
2017-07-27 22:59:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 
 # lms image tag
-VERSION=7.8.1
-DOCKERHUBUSER=heziegl
+#VERSION=7.8.0
+
 
 # docker image to build
-IMAGE_NAME=$DOCKERHUBUSER/rpi-lms:$VERSION
+#IMAGE_NAME=$DOCKERHUBUSER/rpi-lms:$VERSION
 
 # build runtime
-sudo docker build -t $IMAGE_NAME .
+#sudo docker build -t $IMAGE_NAME .
 
 # push to docker hub
-sudo docker push $IMAGE_NAME
+# sudo docker push $IMAGE_NAME 
